fcba2be6f0b17def5ff4b5abe11eb21ff0b81019
oleneveu <oleneveu@gmail.com>
2023-11-01 17:47:20 +0100

    Cleanup

--- NanoframeworkHttpRequestIssue/Configuration.cs
+++ NanoframeworkHttpRequestIssue/Configuration.cs
@@ -2,17 +2,12 @@
 {
     internal static class Configuration
     {
-        public const string WifiSSID = "IOT";
-        public const string WifiPassword = "19761976";
+        public const string WifiSSID = "<yourSSID>";
+        public const string WifiPassword = "<yourpassword>";
 
         /// <summary>
-        /// A random http (non https!) url that accepts a GET request
+        /// Url of the basic http server on your network
         /// </summary>
         public const string NonHttpsUrl = "http://192.168.2.150:5150/";
-            // "http://192.168.2.150:5150/weatherforecast";
-            // "http://192.168.2.140/api/f3DDsr0fdxWMmorIGWyLOYuvmpHB7PJRBLCefQU7/lights/3/state";
-            // "http://192.168.2.136:6789/package-info.json";
-            //"http://192.168.2.140/api/f3DDsr0fdxWMmorIGWyLOYuvmpHB7PJRBLCefQU7/lights/3/state";
-            //"http://data.companieshouse.gov.uk/doc/company/00001419.json";
     }
 }
